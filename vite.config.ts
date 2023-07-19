b4c0a07452f1e95e5a29a10194abe217017915fe
Sérgio Miguel <thesergiomiguel@gmail.com>
2023-07-19 18:42:03 +0200

    Enable aliases & direct import of SVGs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      css: path.resolve(__dirname, "./src/assets/scss/"),
+      img: path.resolve(__dirname, "./src/assets/images"),
+    },
+  },
+});
