5cf4aebe06ade84efc6a9435b89a278b106a6629
Sérgio Miguel <thesergiomiguel@gmail.com>
2023-07-30 10:29:56 +0200

    Add wp banner to gen'd style.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
+/**
+ * @type {import('vite').UserConfig}
+ */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import banner from "vite-plugin-banner";
 import path from "path";
+
+const cssBanner = banner((filename) => {
+  return (
+    filename.match(/\.css$/) &&
+    `/*
+Theme Name: Laulo CFC Child
+Description: Child theme for Laulo CFC
+Author: Sérgio Miguel
+Template: laulo-cfc
+Version: 1.0
+*/`
+  );
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), cssBanner],
   resolve: {
     alias: {
       css: path.resolve(__dirname, "./src/assets/scss/"),
