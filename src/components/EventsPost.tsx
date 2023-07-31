c986eb9a7f418386e3ec39deeadee6334c8bd796
Sérgio Miguel <thesergiomiguel@gmail.com>
2023-07-31 12:42:40 +0200

    Show thumbnails in Events

--- src/components/EventsPost.tsx
+++ src/components/EventsPost.tsx
@@ -8,7 +8,9 @@ export function EventsPost(props: Props) {
   return (
     <article className="EventsPost">
       <div className="EventsPost__content">{props.content}</div>
-      <div className="EventsPost__thumbnail">{props.thumbnail}</div>
+      <div className="EventsPost__thumbnail">
+        <img src={props.thumbnail} />
+      </div>
       <div className="EventsPost__timestamp">{props.timestamp}</div>
 
       <a href="/permalink-to-post">Permalink</a>
