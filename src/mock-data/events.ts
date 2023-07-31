1042cdf9a75e3fa71afdabfe0e30331248546b18
Sérgio Miguel <thesergiomiguel@gmail.com>
2023-07-31 10:56:15 +0200

    Add mock events, members

---
+++ src/mock-data/events.ts
@@ -0,0 +1,26 @@
+export const events = [
+  {
+    content:
+      "OPEN CALL Q&A WITH RIE ALKEMADE: INSIGHTS ON THE 2023 GLOBAL CULTURAL RELATIONS PROGRAMME",
+    thumbnail: "",
+    timestamp: "July 29, 2023",
+  },
+  {
+    content:
+      "WHO CAN RECEIVE GRANT TO SUPPORT THE LATIN AMERICAN ARTISTIC FIELD? Q&A WITH FUNDACIÓN AMA AMOEDO",
+    thumbnail: "",
+    timestamp: "August 29, 2023",
+  },
+  {
+    content:
+      "WHO SHOULD APPLY FOR UNIDEE RESIDENCY MODULES AT CITTADELLARTE-FONDAZIONE PISTOLETTO?",
+    thumbnail: "",
+    timestamp: "August 30, 2023",
+  },
+  {
+    content:
+      "WHAT MAKES A GREAT CURATOR OF EXHIBITIONS? OPEN CALL Q&A WITH NOTTINGHAM CONTEMPORARY",
+    thumbnail: "",
+    timestamp: "July 29, 2023",
+  },
+];
---
+++ src/mock-data/members.ts
@@ -0,0 +1,44 @@
+export const members = [
+  {
+    name: "Some Person's Name",
+    biography:
+      "El mítico Gato Pobre se convirtió en un delincuente profesional. No se sabe nada más.",
+    thumbnail: "",
+    location: "Barcelona, Spain",
+  },
+  {
+    name: "Madonna",
+    biography:
+      "Carlos Ray “Chuck” Norris (born March 10, 1940) is an American martial art and celebrity. He served in the United States Air Force. He became famous as a martial artist and started his own school, Chun Kuk Do. Norris was cast in a number of action…",
+    thumbnail: "",
+    location: "New York, USA",
+  },
+  {
+    name: "Katy Perry",
+    biography:
+      "Peggy Guggenheim was born in New York on August 26, 1898, the daughter of Benjamin Guggenheim and Florette Seligman. Benjamin Guggenheim was one of seven brothers who, with their father Meyer (of Jewish Swiss and German…",
+    thumbnail: "",
+    location: "Tokyo, Japan",
+  },
+  {
+    name: "Lady Gaga",
+    biography:
+      "Immanuel Kant, (born April 22, 1724, Kaliningrad, Russia) is a German philosopher whose comprehensive and systematic work in epistemology (the theory of knowledge), ethics, and aesthetics greatly influenced all subsequent…",
+    thumbnail: "",
+    location: "Rio de Janeiro, Brazil",
+  },
+  {
+    name: "Taylor Swift",
+    biography:
+      "Uma Karuna Thurman (Boston, 29 de abril de 1970) é uma atriz norte-americana vencedora de um Globo de Ouro e indicada para um Óscar. Ficou famosa por interpretar papéis destacados nos filmes do diretor-escritor Quentin Tarantino, como…",
+    thumbnail: "",
+    location: "Lisbon, Portugal",
+  },
+  {
+    name: "Britney Spears",
+    biography:
+      "Immanuel Kant, (born April 22, 1724, Kaliningrad, Russia) is a German philosopher whose comprehensive and systematic work in epistemology (the theory of knowledge), ethics, and aesthetics greatly influenced all subsequent…",
+    thumbnail: "",
+    location: "Rome, Italy",
+  },
+];
