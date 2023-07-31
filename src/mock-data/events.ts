648ab49f8e7b082c0deec43ab271b85528470faf
Sérgio Miguel <thesergiomiguel@gmail.com>
2023-07-31 12:41:52 +0200

    Add thumbnails to mock data

--- src/mock-data/events.ts
+++ src/mock-data/events.ts
@@ -2,25 +2,25 @@ export const events = [
   {
     content:
       "OPEN CALL Q&A WITH RIE ALKEMADE: INSIGHTS ON THE 2023 GLOBAL CULTURAL RELATIONS PROGRAMME",
-    thumbnail: "",
+    thumbnail: "https://picsum.photos/309/221",
     timestamp: "July 29, 2023",
   },
   {
     content:
       "WHO CAN RECEIVE GRANT TO SUPPORT THE LATIN AMERICAN ARTISTIC FIELD? Q&A WITH FUNDACIÓN AMA AMOEDO",
-    thumbnail: "",
+    thumbnail: "https://picsum.photos/290/210",
     timestamp: "August 29, 2023",
   },
   {
     content:
       "WHO SHOULD APPLY FOR UNIDEE RESIDENCY MODULES AT CITTADELLARTE-FONDAZIONE PISTOLETTO?",
-    thumbnail: "",
+    thumbnail: "https://picsum.photos/297/235",
     timestamp: "August 30, 2023",
   },
   {
     content:
       "WHAT MAKES A GREAT CURATOR OF EXHIBITIONS? OPEN CALL Q&A WITH NOTTINGHAM CONTEMPORARY",
-    thumbnail: "",
+    thumbnail: "https://picsum.photos/291/184",
     timestamp: "July 29, 2023",
   },
 ];
