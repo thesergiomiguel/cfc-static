cf824fed8f9862328d9e81d88b9f4af7f079cfb1
Sérgio Miguel <thesergiomiguel@gmail.com>
2023-07-31 10:48:53 +0200

    Rename PodcastPost and use mocked data

--- src/pages/editorial/PodcastPost.tsx
+++ src/pages/editorial/PodcastPost.tsx
@@ -1,17 +1,14 @@
-import { ReactComponent as Play } from "img/icons/play-arrow.svg";
-import "./EditorialPodcastPost.scss";
-
 type Props = {
   timestamp: string;
   content: string;
 };
 
-export function EditorialPodcastPost(props: Props) {
+export function PodcastPost(props: Props) {
   return (
     <article className="PodcastPost">
       <aside className="PodcastPost__aside">
         <a href="/link-to-podcast" className="PodcastPost__play">
-          <Play />
+          Play
         </a>
       </aside>
 
